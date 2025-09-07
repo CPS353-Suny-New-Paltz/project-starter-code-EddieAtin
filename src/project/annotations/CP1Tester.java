963c047e0b955c4e92dd9998b098be25a2d3757f
EddieAtin <148039899+EddieAtin@users.noreply.github.com>
2025-09-07 15:20:21 -0400

    Update CP1Tester.java

--- src/project/annotations/CP1Tester.java
+++ src/project/annotations/CP1Tester.java
@@ -3,6 +3,5 @@ public class CP1Tester {
 	public static void main(String[]args) {
 		System.out.println("HelloWorld");
 		System.out.println("Yeah");
-		System.out.println("Correct");
-	}
+		System.out.println("fail 2	}
 }
