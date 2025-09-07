c3c875e02cd911710aa9fd39f90533809244ecae
EddieAtin <148039899+EddieAtin@users.noreply.github.com>
2025-09-07 14:40:06 -0400

    fail 1

--- src/project/annotations/CP1Tester.java
+++ src/project/annotations/CP1Tester.java
@@ -3,6 +3,6 @@ public class CP1Tester {
 	public static void main(String[]args) {
 		System.out.println("HelloWorld");
 		System.out.println("Yeah");
-		System.out.println("Butter");
+		System.out.println
 	}
 }
