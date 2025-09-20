e99a5583972ea9462ffcd58dc61b58e2870708d0
EddieAtin <148039899+EddieAtin@users.noreply.github.com>
2025-09-20 15:45:03 -0400

    processAPI fix

--- src/api/ProcessAPIProto.java
+++ src/api/ProcessAPIProto.java
@@ -3,7 +3,7 @@ import project.annotations.ProcessAPIPrototype;
 
 public class ProcessAPIProto{
 	@ProcessAPIPrototype
-	public void PrototypeProcess(DataProcess process){
+	public void processAPI(DataProcess process){
 		// take in the input
 		ReadInput readInput = process.input(new ReadInput());
 
