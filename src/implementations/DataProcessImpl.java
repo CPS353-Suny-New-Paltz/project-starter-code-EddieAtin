21cd306bbd12aa046d62dde9ce6a3820a51c8db8
EddieAtin <148039899+EddieAtin@users.noreply.github.com>
2025-12-15 06:02:05 -0500

    Dumb fix

--- src/implementations/DataProcessImpl.java
+++ src/implementations/DataProcessImpl.java
@@ -4,23 +4,17 @@ import api.DataCheck;
 import api.DataProcessAPI;
 import api.ReadRequest;
 import api.ReadResponse;
-import java.util.ArrayList;
-
-import api.DataProcessAPI;
-import api.ReadInput;
 import api.WriteOutput;
 
 public class DataProcessImpl implements DataProcessAPI{
 	
 	@Override
 	public ReadResponse input(ReadRequest request) {
-	public ReadInput input(ArrayList<Integer> sendList) {
 		return null;
 	}
 	
 	@Override
 	public WriteOutput sendData(DataCheck data) {
-	public WriteOutput sendData(ArrayList<Integer> dataOut) {
 		return null;
 	}
 }
