c02a2404ba0048dd6223d8a206b26304fb567870
EddieAtin <148039899+EddieAtin@users.noreply.github.com>
2025-12-15 23:27:04 -0500

    Fixing Checkstyle

--- src/implementations/ComputeEngineImpl.java
+++ src/implementations/ComputeEngineImpl.java
@@ -53,23 +53,14 @@ public class ComputeEngineImpl implements ComputerAPI {
 		result.add(x);
 
 		//perform calculation until x == 1 (complete computation) 
-		while(x != 1) {
-			
-			//check if x is even
-			if(x % 2 == 0) {
-				
-				//collatz sequence requires even integers to be divided by 2
-				x = x/2;
-				result.add(x);
-				//check if odd
-				
-			} else if(x % 2 == 1 ) { 
-				
-				//collatz sequence requires odd integers to be multiplied by 3 and add 1
-				x = (x * 3) + 1;
-				result.add(x);
-				
-			}
+		while (x != 1) {
+		    if (x % 2 == 0) {
+		        x = x / 2;
+		        result.add(x);
+		    } else if (x % 2 == 1) {
+		        x = (x * 3) + 1;
+		        result.add(x);
+		    }
 		}
 
 		return result;
--- test/tests/FuzzTestingConceptual.java
+++ test/tests/FuzzTestingConceptual.java
@@ -17,17 +17,17 @@ private static final int NUM_RUNS = 1000;
 		}
 	@Test
 	public void testFuzzyAdding() {
-		//TODO
-		long seed = System.currentTimeMillis();
-		Random rng = new Random(seed);
-		System.out.println(seed);
-		
-		//AddingMachine toTest = new AddingMachine();
-		for(int i = 0 ; i < NUM_RUNS; i++) { 
-			
-			int a = rng.nextInt();
-			int b = rng.nextInt();
-			//checkComputation(a,b,toTest);
-		}
+	    // TODO
+	    long seed = System.currentTimeMillis();
+	    Random rng = new Random(seed);
+	    System.out.println(seed);
+
+	    // AddingMachine toTest = new AddingMachine();
+	    for (int i = 0; i < NUM_RUNS; i++) {
+	        int a = rng.nextInt();
+	        int b = rng.nextInt();
+	        // checkComputation(a, b, toTest);
+	    }
 	}
+
 }
